78683663ab31616395994a3e6e5155b119c624c3
Daniel Won <dan.j.won@gmail.com>
2021-10-12 05:57:18 -0500

    add date, text, avatar props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<CommentDetail author='Sam' />
-			<CommentDetail author='Alex' />
-			<CommentDetail author='Jane' />
+			<CommentDetail
+				author='Sam'
+				timeAgo='Today at 4:45PM'
+				content='Nice!'
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author='Alex'
+				timeAgo='Today at 2:00AM'
+				content='♥'
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author='Jane'
+				timeAgo='Yesterday at 5:00AM'
+				content='Good job!'
+				avatar={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
