fc8752cb3e0b210b2da640ffdfec8e8b116cec53
Daniel Won <dan.j.won@gmail.com>
2021-10-12 20:28:55 -0500

    next CommentDetail.js inside approval card

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<CommentDetail
-				author='Sam'
-				timeAgo='Today at 4:45PM'
-				content='Nice!'
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author='Alex'
-				timeAgo='Today at 2:00AM'
-				content='♥'
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author='Jane'
-				timeAgo='Yesterday at 5:00AM'
-				content='Good job!'
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author='Sam'
+					timeAgo='Today at 4:45PM'
+					content='Nice!'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Alex'
+					timeAgo='Today at 2:00AM'
+					content='♥'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Jane'
+					timeAgo='Yesterday at 5:00AM'
+					content='Good job!'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
