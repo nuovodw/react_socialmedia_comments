e7e3bd76b1386df5087ed8b932b3e4ab07aaa814
Daniel Won <dan.j.won@gmail.com>
2021-10-13 06:29:03 -0500

    edit index.js

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const CommentDetail = (props) => {
 	return (
-		<div className='comment'>
-			<a href='/' className='avatar'>
-				<img alt='avatar' src={props.avatar} />
+		<div className="comment">
+			<a href="/" className="avatar">
+				<img alt="avatar" src={props.avatar} />
 			</a>
-			<div className='content'>
-				<a href='/' className='author'>
+			<div className="content">
+				<a href="/" className="author">
 					{props.author}
 				</a>
-				<div className='metadata'>
-					<span className='date'>{props.timeAgo}</span>
+				<div className="metadata">
+					<span className="date">{props.timeAgo}</span>
 				</div>
-				<div className='text'>{props.content}</div>
+				<div className="text">{props.content}</div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -6,29 +6,29 @@ import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
-		<div className='ui container comments'>
+		<div className="ui container comments">
 			<ApprovalCard>Are you sure you want to exit?</ApprovalCard>
 			<ApprovalCard>
 				<CommentDetail
-					author='Sam'
-					timeAgo='Today at 4:45PM'
-					content='Nice!'
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					content="Nice!"
 					avatar={faker.image.avatar()}
 				/>
 			</ApprovalCard>
 			<ApprovalCard>
 				<CommentDetail
-					author='Alex'
-					timeAgo='Today at 2:00AM'
-					content='♥'
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					content="♥"
 					avatar={faker.image.avatar()}
 				/>
 			</ApprovalCard>
 			<ApprovalCard>
 				<CommentDetail
-					author='Jane'
-					timeAgo='Yesterday at 5:00AM'
-					content='Good job!'
+					author="Jane"
+					timeAgo="Yesterday at 5:00AM"
+					content="Good job!"
 					avatar={faker.image.avatar()}
 				/>
 			</ApprovalCard>
